014167accc582f4d07cdf635abbb4bb7f485f122
Pavel <web.js.ezh@gmail.com>
2023-11-03 22:02:57 +0300

    add makeAstTree

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import fs from 'fs';
 import path from 'path';
 import { getParsedFileData } from './parser.js';
 import process from 'process';
+import _ from 'lodash'
 
 const getAbsolutePath = (file) => {
     const absolutePath = path.resolve(process.cwd(), file);
-    return absolutePath; 
+    return absolutePath;
 };
 const getExtName = (file) => path.extname(file);
 const readData = (file) => fs.readFileSync(file, 'utf-8');
@@ -13,17 +14,19 @@ const readData = (file) => fs.readFileSync(file, 'utf-8');
 
 const genDiff = (filepath1, filepath2, formatName) => {
     const absolutPath1 = getAbsolutePath(filepath1)
-    console.log(absolutPath1)
     const absolutPath2 = getAbsolutePath(filepath2)
     const data1 = readData(absolutPath1)
-    console.log(data1)
     const data2 = readData(absolutPath2)
     const ext1 = getExtName(filepath1)
-    console.log(ext1)
     const ext2 = getExtName(filepath2)
     const parsedData1 = getParsedFileData(data1, ext1)
-    console.log(parsedData1)
     const parsedData2 = getParsedFileData(data2, ext2)
+
+    // const allKeys = _.union(Object.keys(parsedData1), Object.keys(parsedData2))
+    // console.log(allKeys)
+    // const sortedAllKeys = _.sortBy(allKeys)
+
+
     return {parsedData1, parsedData2 }
 }
 
