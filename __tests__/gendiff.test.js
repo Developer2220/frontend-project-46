0a1fb2c9c8ba38964afd14c756884e98ffe8baaa
Pavel <web.js.ezh@gmail.com>
2023-11-13 19:58:40 +0300

    fix parser, add yaml.load

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,8 +9,10 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('check genDiff', () => {
-    const outputPath = path.join(__dirname, 'etalonStylish.txt');
   expect( genDiff(getFixturePath('file1.json'), getFixturePath('file2.json')))
+  .toEqual(fs.readFileSync(getFixturePath('etalon.txt'), 'utf8')
+  );
+  expect( genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yaml')))
   .toEqual(fs.readFileSync(getFixturePath('etalon.txt'), 'utf8')
   );
 });
--- src/parser.js
+++ src/parser.js
@@ -1,8 +1,11 @@
-
+import yaml from 'js-yaml';
 
 const getParsedFileData = (data, ext) => {
     if (ext === '.json') {
         return JSON.parse(data)
+    }
+    if (ext === '.yaml' || ext === '.yml' ) {
+        return yaml.load(data)
     }
 };
 
